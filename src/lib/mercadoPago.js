f52fa5a6480d4c030ca9eb1ebf45dd8b642eeecc
franco <91661089+davidfranncoo@users.noreply.github.com>
2023-04-08 17:47:45 -0300

    mercado

--- src/lib/mercadoPago.js
+++ src/lib/mercadoPago.js
@@ -1,7 +1,9 @@
 const mercadopago = require('mercadopago')
+let c="APP_USR-2534937450399201-040416-08aace381f8e98556b31ae5149d2d419-1345817554"
+
 
 mercadopago.configure({
-    access_token: process.env.MP_ACCESS_TOKEN,
+    access_token: c,
     // client_id: process.env.MP_CLIENT_ID,
     // client_secret: process.env.MP_CLIENT_SECRET
 })
